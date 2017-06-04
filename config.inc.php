45a3d4712e3b162ab3c4fb10182d36d9bedb9060
tonghui110 <tonghui@caixun.com>
2017-06-04 14:45:50 +0800

    123

---
+++ config.inc.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Ucenter config
+ */
+define('UC_CONNECT', 'mysql');
+define('UC_DBHOST', 'phpdbbak.cxjpd74we29t.ap-southeast-1.rds.amazonaws.com');
+define('UC_DBUSER', 'uc');
+define('UC_DBPW', 'frank@)!&');
+
+define('UC_DBNAME', 'dw_uc_user');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', '`dw_uc_user`.uc_');
+define('UC_DBCONNECT', '0');
+define('UC_KEY', '1111111111111111111');
+define('UC_API', 'http://uc-api.dwnews.com');
+define('UC_CHARSET', 'utf-8');
+define('UC_IP', '');
+define('UC_APPID', '5');
+define('UC_PPP', '20');
